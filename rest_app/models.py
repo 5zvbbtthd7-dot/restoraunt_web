7985fcd40071bd68058dc5ec608889cbfa094ac3
zhenya <5zvbbtthd7@privaterelay.appleid.com>
2026-04-01 18:57:06 +0300

    added/models

--- rest_app/models.py
+++ rest_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='dishes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=255)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.dish.name} x {self.quantity}"
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
--- rest_app/views.py
+++ rest_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Dish
 
-# Create your views here.
+def home(request):
+    dishes = Dish.objects.all()[:6]
+    return render(request, 'home.html', {'dishes': dishes})
+
+def menu(request):
+    dishes = Dish.objects.all()
+    return render(request, 'menu.html', {'dishes': dishes})
+
+def dish_detail(request, id):
+    dish = get_object_or_404(Dish, id=id)
+    return render(request, 'dish.html', {'dish': dish})
